0733bed254df8773ee73e0d9d02e561a1946ca42
vivienkp <vivienkp@gmail.com>
2023-07-05 14:11:11 -0400

    define and Index and Show in controllers

--- controllers/skills.js
+++ controllers/skills.js
@@ -4,3 +4,15 @@ module.exports = {
     index, 
     show
 };
+
+function show(req, res) {
+    res.render('skills/show', {
+        skill: Skill.getOne(req.params.id)
+    });
+}
+
+function index(req, res) {
+    res.render('skills/index', {
+      skills: Skill.getAll()
+    });
+  }
