8ed4c48d6a1f034d17b47822c9d92c14799cde2d
vivienkp <vivienkp@gmail.com>
2023-07-06 15:10:18 -0400

    Modified routes & views for a new entry

--- routes/skills.js
+++ routes/skills.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
 
+
+
 //GET request /skills
 router.get('/', skillsCtrl.index);
-
-//Get /todos/:id
+//GET /skills/new 
+router.get('/new', skillsCtrl.new);
+//Get /skills/:id
 router.get('/:id', skillsCtrl.show);
 /*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });*/
+
+//POST /skills
+router.post('/', skillsCtrl.create);
 
 module.exports = router;
